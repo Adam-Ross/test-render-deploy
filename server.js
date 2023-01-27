e413efa08bbe59d00fa5bf7972cfd5d854e824d8
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2023-01-27 10:21:42 -0600

    Testing

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json())
 // get all
 app.get('/pets', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM pet');
+        const result = await pool.query('SELECT * FROM pets');
         res.json(result.rows);
     } catch (err) {
         res.status(500).json({ error: err });
@@ -35,41 +35,41 @@ app.get('/pets', async (req, res) => {
 
 
 // get one route
-app.get('/pets/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const result = await pool.query('SELECT * FROM pet WHERE pet_id = $1', [id]);
-        res.json(result.rows[0]);
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
-});
+// app.get('/pets/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params
+//         const result = await pool.query('SELECT * FROM pet WHERE pet_id = $1', [id]);
+//         res.json(result.rows[0]);
+//     } catch (err) {
+//         res.status(500).json({ error: err });
+//     }
+// });
 
-app.post('/pets', async (req, res) => {
-    try {
-        const { name, age, kind } = req.body;
+// app.post('/pets', async (req, res) => {
+//     try {
+//         const { name, age, kind } = req.body;
         
-        await pool.query('INSERT INTO pet (name, age, kind) VALUES ($1, $2, $3)', [name, age, kind]);
+//         await pool.query('INSERT INTO pet (name, age, kind) VALUES ($1, $2, $3)', [name, age, kind]);
         
-        res.json({ success: true, message: 'Pet added' }).status(201)
-    } catch (err) {
-        res.status(500).json({ error: err });
-    }
-});
+//         res.json({ success: true, message: 'Pet added' }).status(201)
+//     } catch (err) {
+//         res.status(500).json({ error: err });
+//     }
+// });
 
-app.put('/pets/:id', async (req, res) => {
-    try {
+// app.put('/pets/:id', async (req, res) => {
+//     try {
 
-        const { name, age, kind } = req.body;
-        const { id } = req.params
+//         const { name, age, kind } = req.body;
+//         const { id } = req.params
         
-        await pool.query('UPDATE pet SET name = $1, age = $2, kind = $3 WHERE pet_id = $4', [name, age, kind, id]);
+//         await pool.query('UPDATE pet SET name = $1, age = $2, kind = $3 WHERE pet_id = $4', [name, age, kind, id]);
         
-        res.json({  message: `Updated pet_id: ${id}` }).status(204);
-    } catch (err) {
-        res.status(500).json({ err });
-    }
-});
+//         res.json({  message: `Updated pet_id: ${id}` }).status(204);
+//     } catch (err) {
+//         res.status(500).json({ err });
+//     }
+// });
 
 
 
